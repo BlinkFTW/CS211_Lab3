ad0afdcfdfe6dfb5090799af56d922ad3e904960
BlinkFTW <ekang8@gmail.com>
2014-08-28 18:08:28 -0700

    Header file

---
+++ lab3Func.h
@@ -0,0 +1,16 @@
+#ifndef _LAB3FUNC_H
+#define _LAB3FUNC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <ctype.h>
+#include <math.h>
+
+void upConv( char *sPtr );
+int vCount( char *sPtr );
+void swap( char *sPtr1, char *sPtr2 );
+//int swapRet( char *sPtr1, char *sPtr2 );
+
+
+#endif
