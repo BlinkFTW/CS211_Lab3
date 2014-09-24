43dc122304343e0cf43ec47762914f2eb9b91a4d
Eric J. Kang <ekang8@gmail.com>
2014-09-23 17:31:57 -0700

    Update lab3.c
    Completed Assignment

--- lab3.c
+++ lab3.c
@@ -1,23 +1,50 @@
 #include "lab3Func.h"
 
+#define WORDSIZE 16
+#define LISTSIZE 20
+
 int main() {
-	char words[] = "dog";
-	char *chPtr = &words[0];
+	char words[WORDSIZE] = "FISHER";
 	int vowels;
 	
-	upConv(chPtr);
+	/* Test Part B */
+	upConv(words);
+	vowels = vCount(words);
+	printf("There are %d vowels in %s\n", vowels,words);
 	
-	vowels = vCount(chPtr);
-	
-	printf("There are %d vowels\n", vowels);
-	
-	// Test Part C
-	char words2[] = "cat";
+	/* Test Part C */
+	char words2[WORDSIZE] = "FISH";
 	upConv(words2);
 	
-	swap(words,words2);
+	printf( "\nBefore swap:\nWord1 is %s\nWord2 is %s\n", words, words2 );
+	alphOrder(words,words2);
+	printf( "\nAfter swap:\nWord1 is %s\nWord2 is %s\n", words, words2 );
+
+
+	/* Test Part D */
+	int i,j, count = 0;
+	char list[LISTSIZE][WORDSIZE] = { "QUEBEC","PAPA","OSCAR","FOXTROT","INDIA",
+												"KILO","HOTEL","TANGO","NOVEMBER","SIERRA",
+												"ROMEO","ALPHA","ECHO","MIKE","DELTA",
+												"BRAVO","JULIET","GOLF","LIMA","CHARLIE" };
 	
-	printf("Word1 is %s\nWord2 is %s\n",words,words2);
+	printf("Here's the UNsorted list:\n");
+	for(i=0;i<LISTSIZE;i++){
+		printf("%s\n",list[i]);
+	}
+	
+	// Perform Sort
+	for(i=0;i<LISTSIZE;i++){
+		for(j=0;j<LISTSIZE-1;j++){
+			count += alphOrderRet(list[j],list[j+1]);
+		}
+	}
+	
+	printf("\nAfter %d swaps, here's the Sorted list:\n",count);
+	for(i=0;i<LISTSIZE;i++){
+		printf("%s\n",list[i]);
+	}
+	
 
 	return 0;
 }
